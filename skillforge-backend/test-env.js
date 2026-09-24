2f4245560500d924bc9d6620872263a550227e51
Wasem Twfek <121197063+wasem15@users.noreply.github.com>
2026-09-24 22:58:15 +0300

    Remove environment debug helper

--- skillforge-backend/test-env.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
-console.log('GOOGLE_CLIENT_SECRET:', process.env.GOOGLE_CLIENT_SECRET);
-console.log('GOOGLE_REDIRECT_URI:', process.env.GOOGLE_REDIRECT_URI);
-console.log('JWT_SECRET:', process.env.JWT_SECRET);
-console.log('FRONTEND_URL:', process.env.FRONTEND_URL); 
